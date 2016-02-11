a606aa37d64ccde72b457ed07cb74bb77bc88a0d
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-02-11 08:38:41 +0100

    Tried out the class

--- DiceGame/InsertCodeHere.cs
+++ DiceGame/InsertCodeHere.cs
@@ -11,6 +11,14 @@ namespace DiceGame
         {
             // The FIRST line of code should be BELOW this line
 
+            // Creates die object
+            Die die1 = new Die();
+
+
+            die1.RollDie();
+            Console.WriteLine(die1.GetValue());
+
+
 
             // The LAST line of code should be ABOVE this line
         }
